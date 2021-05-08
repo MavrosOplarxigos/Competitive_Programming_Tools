e11597e1abf6db22805adc57a69ae48f5caa55cd
MavrosOplarxigos <74368317+MavrosOplarxigos@users.noreply.github.com>
2021-05-08 20:25:38 +0300

    Fixed the explanation comments

--- Mathematics/fast_sieve_with_explanation.cpp
+++ Mathematics/fast_sieve_with_explanation.cpp
@@ -20,9 +20,14 @@ void sieve(int N){
 	// LOOPS TO CALCULATE THE NUMBERS
 	
 	// the most naive way
-	/*for(int i=2;i<=N;i++){
-		
+	// we just go through all the numbers and for each check if all the previous numbers from 2 to i-1 do not divide i
+	
+	/*
+	for(int i=2;i<=N;i++){
+	
+		// inner loop to check if i is prime
 		for(int j=2;j<i;j++){
+			// if modulo equals to 0 then j divides i
 			if(i % j == 0){
 				is_not_prime[i] = 1;
 				break;
@@ -33,30 +38,54 @@ void sieve(int N){
 			primes.push_back(i);
 		}
 
-	}*/
+	}
+	*/
 
 	// level 1 sieve
-	/*for(int i=2;i<=N;i++){
+	// we use the sieve of eratosthenes without any optimization
+	
+	/*
+	for(int i=2;i<=N;i++){
+	
+		// if the number is prime then we start erasing all the subsequent numbers that it divides
 		if(is_not_prime[i] == 0){
+		
 			primes.push_back(i);
+			
+			// we start from the number that is immediatly after i and is divided by i -> 2 * i
 			for(int j=2*i;j<=N;j+=i){
 				is_not_prime[j] = 1;
 			}
+			
 		}
-	}*/
+		
+	}
+	*/
 
 	// level 2 sieve
-	/*primes.push_back(2);
+	// we notice the even numbers can be just skipped
+	
+	/*
+	primes.push_back(2);
 	for(int i=3;i<=N;i+=2){
+	
 		if(is_not_prime[i] == 0){
+		
 			primes.push_back(i);
+			
 			for(int j=2*i;j<=N;j+=i){
 				is_not_prime[j] = 1;
 			}
+			
 		}
-	}*/
+		
+	}
+	*/
 
 	// level 3 sieve
+	// we notice that we don't need to start erasing from 2 * i but instead we can start from i*i
+	// that is because we already erased all the numbers divided by primes smaller than i
+	
 	primes.push_back(2);
 	for(int i=3;i<=N;i+=2){
 		if(is_not_prime[i] == 0){
@@ -91,8 +120,8 @@ int main(){
 	ios::sync_with_stdio(false);
   	cin.tie(0);
 
-  						// N 		primes until N:
-  	sieve(10); 			// 10^1		4
+  				// N 		primes until N:
+  	sieve(10); 		// 10^1		4
   	sieve(100); 		// 10^2		25
   	sieve(1000); 		// 10^3		168
   	sieve(10000); 		// 10^4		1229
