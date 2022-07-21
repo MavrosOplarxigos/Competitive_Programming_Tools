f9acf18063c3197a6fe910a98e84af4d2d009361
MavrosOplarxigos <r4fail.loizou@gmail.com>
2022-07-21 02:21:05 +0200

    added odd or even check
    Signed-off-by: MavrosOplarxigos <r4fail.loizou@gmail.com>

---
+++ data_structures/custom_class_pq.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// < (less than operator)
+class ltclass{
+public:
+    int a,b,c; // b+c
+};
+
+class compclass{
+public:
+    bool operator()(const ltclass & left, const ltclass & right){
+        return left.b+left.c < right.b+right.c;
+    }
+};
+
+bool mcomp(const ltclass left, const ltclass right){
+    return left.b+left.c < right.b+right.c;
+}
+
+int main(){
+
+    srand(time(NULL));
+
+    priority_queue < ltclass, vector < ltclass >, 
+    function < bool(ltclass,ltclass) > > ltpq(mcomp);
+
+    for(int i=0;i<10;i++){
+        ltclass curr;
+        curr.a = rand() % 20;
+        curr.b = rand() % 20;
+        curr.c = rand() % 20;
+        ltpq.push(curr);
+    }
+
+    while(!ltpq.empty()){
+        cout << ltpq.top().b + ltpq.top().c << endl;
+        ltpq.pop();
+    }
+
+
+}
---
+++ simple_problems/odd_or_even.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define isodd(i) (i%2)
+using namespace std;
+
+int main(){
+
+	int x;
+	cin >> x;
+
+
+	if( isodd(x) ){
+		cout << "Odd" << endl;
+		return 0;
+	}
+
+	cout << "Even" << endl;
+
+
+}
