ab5beb4f980eb98fe6bdcff63520db0d1733eeb3
MavrosOplarxigos <r4fail.loizou@gmail.com>
2023-03-23 23:58:59 +0100

    more fixes

--- contest_dir/control/template.cpp
+++ contest_dir/control/template.cpp
@@ -1,25 +1,32 @@
 #include<bits/stdc++.h>
 #define ll long long
 #define ld long double
-//#define M_PI (2*acos(0))
-#define MAX_N 10001
-#define MOD 1000000007 // 10^9 + 7
 #define apo(a,b) sqrtl( powl(a.x-b.x,2) + powl(a.y-b.y,2) )
 #define ldeq(a,b) ( fabsl(a - b) < 0.00000000007 ) // check long double rough equality 
 #define rep(a,b) for(int i=a; i<=b; i++)
 #define repv(a,b,i) for(int i=a; i<=b; i++)
-#define mmod(a,b) ( (a >= 0) ? ((a%b)%b) : ( ( ( (b-1) * ( (-a) % b ) ) % b ) % b )  ) // -a mod b = -1 mod b * a mod b = b-1 mod b * a mod b
+#define mmod(a,b) ( (a >= 0) ? (a%b) : ( ( (b-1) * ( (-a) % b ) ) % b )  )
 #define ALL(x) x.begin(),x.end()
 #define S(x) ((int)(x.size()))
 #define PB push_back
-#define sum_f1tx(x) ( (x*(x+1)) / 2 )
-#define sum_fxty(x,y) ( ( ( (y-x) + 1 ) * ( x + y ) ) / 2 )
+#define sum_f1tx(x) ( ( x * (x+1) ) / 2 )
+#define sum_fxty(x,y) ( ( ( ( y - x ) + 1 ) * ( x + y ) ) / 2 )
+
+// --------------- PARAMETERS -----------------
+#define MULTITEST 1 // 0 for single tests, 1 for multi-test
+//#define M_PI (2*acos(0))
+#define MAX_N 10001
+#define MOD 1000000007 // 10^9 + 7
 #define MAXSIEVE 1
 // Uncomment the next two lines when you want to use the sieve
 //#undef MAXSIEVE
 //#define MAXSIEVE (1000000000LL+1)
 using namespace std;
+
+// ----------------- CUSTOM FUNCTION DECLARATIONS -----------------
 vector < ll > sieve(ll N);
+
+// >>>>>>>>>>>>>>>>>> SOLUTION BEGIN <<<<<<<<<<<<<<<<<<<<<
 
 void doit(){
 
@@ -27,13 +34,17 @@ void doit(){
 	
 }
 
+// >>>>>>>>>>>>>>>>>> SOLUTION END <<<<<<<<<<<<<<<<<<<<<
+
 int main(){
 
 	ios::sync_with_stdio(false);
   	cin.tie(0);
   	
 	int t = 1;
-	// cin >> t; // uncomment for multi-tests
+	if( MULTITEST ){
+		cin >> t;
+	}
 
 	while(t--){
 		doit();
@@ -41,6 +52,7 @@ int main(){
 
 }
 
+// -------------------------- CUSTOM FUNCTION DEFINITIONS -------------------------
 bool is_not_prime[MAXSIEVE]; 
 vector < long long > sieve(long long N){
 
