c4992eb048817b9112f9d18f514b1b7467aad867
MavrosOplarxigos <r4fail.loizou@gmail.com>
2023-06-05 06:33:16 +0200

    fixes after some contructive number theory training

--- contest_dir/control/template.cpp
+++ contest_dir/control/template.cpp
@@ -34,11 +34,15 @@
 using namespace std;
 
 // ----------------- CUSTOM FUNCTION DECLARATIONS -----------------
+bool is_not_prime[MAXSIEVE]; 
+vector < ll > primes;
 vector < ll > sieve(ll N);
 
 // >>>>>>>>>>>>>>>>>> SOLUTION BEGIN <<<<<<<<<<<<<<<<<<<<<
 
 // Check before submission these common mistakes:
+// - To reveal the "extra prime factor powers" that a number X has compared to some number Y just do " X / __gcd(X,Y) "
+// - For a number X there "can" be "Y<X" such that for a "certain prime factor Y has a higher power" (e.g. X=6=2*3 and Y=4=2*2)
 // - For N>4 there is alway a permutation with abs(p[i],p[i+1]) > 1 for all 1 <= i <= N.
 // - For integer X != 1 and P prime -> X * P is not prime because more than 1 prime factors.
 // - That you "didn't confuse underlines in the example case with overlines in the instruction" that are meaning element inversion most propably.
@@ -50,8 +54,8 @@ vector < ll > sieve(ll N);
 // - That you didn't have a case of "bitwise operations not nested" like "( a & b > 0 )". NEST THE OPERATORS THAT YOU DON'T KNOW FOR CERTAIN.
 // - "Arrays with size >400K bytes should be global" (for variable sizes use vectors; little bit of speed loss >> WA because of stack memory being so small)
 // - "Read the whole code one more time." Reading should take < 1 min so you won't loose much rating "(for after C)".
-// - We "don't use rep no more" to ensure that we use "long longs in the cases where indexes take such large values".
- 
+// - We "don't use rep no more" to ensure that we use "long longs in the cases where indexes take such large values". 
+
 void doit(){
 
 	
@@ -64,7 +68,8 @@ int main(){
 
 	ios::sync_with_stdio(false);
   	cin.tie(0);
-  	
+  	sieve(MAXSIEVE);
+	
 	int t = 1;
 	if( MULTITEST ){
 		cin >> t;
@@ -77,15 +82,20 @@ int main(){
 }
 
 // -------------------------- CUSTOM FUNCTION DEFINITIONS -------------------------
-bool is_not_prime[MAXSIEVE]; 
 vector < long long > sieve(long long N){
 
 	vector < long long > primes;
 
+	if( N == 1){
+		return primes;
+	}
+
 	is_not_prime[1] = is_not_prime[0] = 1;
 	
 	primes.push_back(2);
+	if( N % 2 == 0 ){ is_not_prime[N] = 1; }
 	for(long long i=3;i<=N;i+=2){
+		if( i>3 ){ is_not_prime[i-1] = 1; }
 		if(is_not_prime[i] == 0){
 			primes.push_back(i);
 			for(long long j=i*i;j<=N;j+=i){
